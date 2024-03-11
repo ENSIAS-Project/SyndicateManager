0b8120ed4616edbef467e54e86745d3243072265
EL BOUZIYANI Anas <aaelbouziyani@gmail.com>
2024-03-11 18:25:37 +0000

    add UI
    - added ui
    - fix gradle implementation
    - add gitignore

--- AndroidProject/app/build.gradle.kts
+++ AndroidProject/app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(platform(libs.firebase.bom))
+    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- AndroidProject/app/src/main/java/com/ensias/syndicatemanager/MainActivity.kt
+++ AndroidProject/app/src/main/java/com/ensias/syndicatemanager/MainActivity.kt
@@ -1,46 +1,76 @@
-package com.ensias.syndicatemanager
+    package com.ensias.syndicatemanager
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ensias.syndicatemanager.ui.theme.SyndicateManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SyndicateManagerTheme {
+           SyndicateManagerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LoginView()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LoginView(){
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Bottom),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextInput()
+        TextInput()
+        Button(onClick={},modifier = Modifier.fillMaxWidth()){
+            Text("LOGIN", Modifier.padding(vertical = 8.dp))
+        }
+        Divider(modifier = Modifier.padding(60.dp))
+    }
+}
+@Composable
+fun TextInput(){
+    var value:String by remember { mutableStateOf("") }
+    
+    TextField(value = value, onValueChange = {value = it})
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LoginPreview() {
     SyndicateManagerTheme {
-        Greeting("Android")
+        LoginView()
     }
 }
