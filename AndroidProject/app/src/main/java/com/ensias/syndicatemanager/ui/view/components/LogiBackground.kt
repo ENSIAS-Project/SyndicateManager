8c5fb380cdd8ce735afa45940de463fa1bf1fbc7
EL BOUZIYANI Anas <aaelbouziyani@gmail.com>
2024-04-24 00:48:44 +0100

    fix a string value
    progress on the repport

--- AndroidProject/app/src/main/java/com/ensias/syndicatemanager/ui/view/components/LogiBackground.kt
+++ AndroidProject/app/src/main/java/com/ensias/syndicatemanager/ui/view/components/LogiBackground.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
